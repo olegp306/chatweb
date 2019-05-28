9ef5e63a6c907d988ef80727b8863177f81d45c2
oleg <olegp306@gmail.com>
2019-05-28 22:29:46 +0300

    clean

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './ReactotronConfig'
-//import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
 
 window.GermesChat=App;
@@ -37,5 +34,5 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
 
