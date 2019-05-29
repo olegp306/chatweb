826ba7d26defabd52ac0ace9fd0830dfb677b980
oleg <olegp306@gmail.com>
2019-05-30 00:51:25 +0300

    add styles and pass new data

--- src/components/Message/Message.js
+++ src/components/Message/Message.js
@@ -3,58 +3,48 @@ import React, { Component } from "react";
 import AvatarHelper from "../../utils/avatarHelper";
 
 export default class Message extends Component {
-  componentWillReceiveProps(nextProps) {
-    //   this.setState({
-    //     messageInfo:nextProps.messageInfo,
-    //     isMyMessage:nextProps.isMyMessage,
-    //     isNewMessage:nextProps.isNewMessage,
-    //     userInfo:nextProps.userInfo });
-  }
-
-  getUserPhoto(username) {
-    let avatar=new AvatarHelper();
+  getUserPhoto = username => {
+    let avatar = new AvatarHelper();
     return avatar.getUserPhoto(username);
-  }
+  };
 
   render() {
     const { author, message, isMyMessage, isNewMessage } = this.props;
+    const messageDateTime = new Date(message.creationDate).toLocaleTimeString();
     return (
       <div className={isNewMessage == true ? "message new-message" : "message"}>
         <li
           className={isMyMessage == true ? "right clearfix" : "left clearfix"}
         >
-          <span
+          <div
             className={
               isMyMessage == true ? "chat-img pull-right" : "chat-img pull-left"
             }
           >
             <img
-              src={author ?this.getUserPhoto(author.name) :  this.getUserPhoto("???")}
+              src={
+                author
+                  ? this.getUserPhoto(author.name)
+                  : this.getUserPhoto("???")
+              }
               alt="User Avatar"
               className="img-circle"
             />
-          </span>
-          <div className="chat-body clearfix">
-            <div className="header">
-              <strong
-                className={
-                  isMyMessage == true
-                    ? "primary-font pull-right"
-                    : "primary-font"
-                }
-              >
-                {author ? author.name : "неизвестный"}
-              </strong>
-              <small
-                className={
-                  isMyMessage == true ? "text-muted" : "pull-right text-muted"
-                }
-              >
-                <span className="glyphicon glyphicon-time " />
-                {message.creationDate}
-              </small>
+          </div>
+          <div className="chat-body clearfix ">
+            <div className="message-author">
+              {author ? author.name : "неизвестный"} {messageDateTime}
             </div>
-            <p>{message.text}</p>
+
+            <div
+              className={
+                isMyMessage == true
+                  ? "my-message-text-block"
+                  : "message-text-block"
+              }
+            >
+              {message.text}
+            </div>
           </div>
         </li>
       </div>
