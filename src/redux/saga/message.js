597f3c0f82fcd8496071be8f284ecc498d7f99db
oleg <olegp306@gmail.com>
2019-05-31 12:43:33 +0300

    add cleanMessage in newMessages in sendmessage saga

--- src/redux/saga/message.js
+++ src/redux/saga/message.js
@@ -1,27 +1,31 @@
 import { call, put, select } from "redux-saga/effects";
 
 import { add, isAdding, addSuccess, addFail } from "../actions/message";
+import { cleanNewMessage } from "../actions/newMessages";
 import api from "../../api";
 import { getSession, getCurrentChat, getNewMessages } from "../selectors";
 
 function* addMessageSaga() {
   yield put(isAdding());
 
-  const store = yield select()
-  const chat=getCurrentChat(store);
-  const newMessages=getNewMessages(store);
-  const messageText=newMessages.items[chat.id].message
+  const store = yield select();
+  const chat = getCurrentChat(store);
+  const newMessages = getNewMessages(store);
+  const messageText = newMessages.items[chat.id].message;
   try {
-      const message = {
-      chatId: chat.id,      
+    const message = {
+      chatId: chat.id,
       text: messageText,
       type: 2768777882000,
-      tempFrontId: new Date()+ chat.id + messageText
+      tempFrontId: new Date() + chat.id + messageText
     };
 
     const response = yield call(api.addMessage, message);
 
     yield put(addSuccess(response.data));
+
+    yield put(cleanNewMessage(response.data.chatId));
+
   } catch (error) {
     yield put(addFail(error));
   }
