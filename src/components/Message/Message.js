05194a68ff242c7a0f53a7c8820bf70738b8cf53
oleg <olegp306@gmail.com>
2019-06-03 13:26:43 +0300

    add pictures type of message for  message comp

--- src/components/Message/Message.js
+++ src/components/Message/Message.js
@@ -13,27 +13,34 @@ export default class Message extends Component {
     const messageDateTime = new Date(message.creationDate).toLocaleTimeString();
     return (
       <div className={isNewMessage == true ? "message new-message" : "message"}>
-        
+        <div
+          className={
+            isMyMessage == true ? "chat-img pull-right" : "chat-img pull-left"
+          }
+        >
+          <img
+            src={
+              author ? this.getUserPhoto(author.name) : this.getUserPhoto("???")
+            }
+            alt="User Avatar"
+            className="img-circle"
+          />
+        </div>
+        <div className="chat-body clearfix ">
           <div
             className={
-              isMyMessage == true ? "chat-img pull-right" : "chat-img pull-left"
+              isMyMessage == true ? "my-message-author" : "message-author"
             }
           >
-            <img
-              src={
-                author
-                  ? this.getUserPhoto(author.name)
-                  : this.getUserPhoto("???")
-              }
-              alt="User Avatar"
-              className="img-circle"
-            />
+            {author ? author.name : "неизвестный"} {messageDateTime}
           </div>
-          <div className="chat-body clearfix ">
-            <div className= {isMyMessage == true ? "my-message-author" : "message-author"}>
-              {author ? author.name : "неизвестный"} {messageDateTime}
-            </div>
 
+          {message.type == 2768654243000 ? ( //картинка
+            //<Image style={{width: 50, height: 50}} source={text} />
+            <img src={message.url} alt="альтернативный текст"></img>
+            // <Image style={{ width: 100, height: 100 }} source={{ uri: url }} />
+          ) : (
+            //  <Image source={{ uri: url }} style={{ width: 100, height: 100 }} />
             <div
               className={
                 isMyMessage == true
@@ -43,8 +50,8 @@ export default class Message extends Component {
             >
               {message.text}
             </div>
-          </div>
-        
+          )}
+        </div>
       </div>
     );
   }
