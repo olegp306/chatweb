3bcc2d1ad57d2fb95aa6f3c2c4bc4cd350c01c8f
oleg <olegp306@gmail.com>
2019-06-07 14:01:27 +0300

    new style for add button

--- src/components/UsersList/UsersList.js
+++ src/components/UsersList/UsersList.js
@@ -1,35 +1,41 @@
 import React, { Component } from "react";
-import UserSmall from "../User/UserSmall/UserSmall"
-import UserForAdd from "../User/UserForAdd/UserForAdd"
+import UserSmall from "../User/UserSmall/UserSmall";
+import UserForAdd from "../User/UserForAdd/UserForAdd";
 
-export default class UsersList extends Component { 
+export default class UsersList extends Component {
+  //при инициализации 1 раз
+  componentDidMount() {
+    window.$(document).on("click", ".add-user-in-chat-panel", function(e) {
+      e.stopPropagation();
+    });
+  }
 
-    //при инициализации 1 раз
-    componentDidMount() {
-      window.$(document).on('click','.add-user-in-chat-panel',function(e){e.stopPropagation();});
-    }
-  
-    componentWillUnmount() {  
-      window.$(document).off('click','.add-user-in-chat-panel',function(e){e.stopPropagation();});
-    }
+  componentWillUnmount() {
+    window.$(document).off("click", ".add-user-in-chat-panel", function(e) {
+      e.stopPropagation();
+    });
+  }
 
   renderUsersList = usersItems => {
-    const{onClickUser,selectedUsers}=this.props;
+    const { onClickUser, selectedUsers } = this.props;
 
     return usersItems.map((item, index) => {
       return (
         <UserForAdd
           user={item}
-          isSelected={selectedUsers.get(item.id) && selectedUsers.get(item.id)==true ? true: false  }
-          onClick={()=>onClickUser(item.id)}
-
+          isSelected={
+            selectedUsers.get(item.id) && selectedUsers.get(item.id) == true
+              ? true
+              : false
+          }
+          onClick={() => onClickUser(item.id)}
         />
       );
     });
   };
 
   render() {
-    const { users, selectedUsers } = this.props;
+    const { users, selectedUsers, addUsers, addUserstoChat } = this.props;
 
     return (
       <div className="panel panel-primary chats-panel">
@@ -37,6 +43,13 @@ export default class UsersList extends Component {
           <h3 className="panel-title">
             Добавьте участников
             {/* <button className="btn btn-primary refresh-btn" onClick={this.props.updateDataFn}><span className="glyphicon glyphicon-refresh"></span> </button>*/}
+            <button
+              type="button"
+              className="btn btn-primary user-list-submit-selection" 
+              onClick={addUserstoChat}
+            >
+              готово
+            </button>
           </h3>
           <div className="input-group search-users-input">
             <input
@@ -51,22 +64,23 @@ export default class UsersList extends Component {
               </span>*/}
           </div>
         </div>
-        <div className="panel-body chat-list add-user-in-chat-panel">        
-
+        <div className="panel-body chat-list add-user-in-chat-panel">
           <div className="sidebar">
             {/* <span className="label label-default">Доступные контакты</span> */}
-            <ul className="nav nav-sidebar chat-list-grid">{this.renderUsersList(users.items)}</ul>
+            <ul className="nav nav-sidebar chat-list-grid">
+              {this.renderUsersList(users.items)}
+            </ul>
           </div>
         </div>
-        <div className="add-users-panel-bottom">
+        {/* <div className="add-users-panel-bottom">
         <button
           type="button"
           className="btn btn-primary"
-          onClick={this.addUsers}
+          onClick={addUserstoChat}
         >
           Добавить
         </button>
-        </div>
+        </div> */}
       </div>
     );
   }
