458076b9ad4de12ba34d4a426b99f6ea58dbf97a
oleg <olegp306@gmail.com>
2019-06-07 14:02:32 +0300

    add loading behavior and impl user for add filter

--- src/containers/UsersListContainer/UsersListContainer.js
+++ src/containers/UsersListContainer/UsersListContainer.js
@@ -2,22 +2,20 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import UsersList from "../../components/UsersList/UsersList";
 import { selectUser, unSelectUser } from "../../redux/actions/usersList";
-import _ from "lodash"
+import { add as addUserstoChat } from "../../redux/actions/usersList";
+import _ from "lodash";
 
 import {
   getUsers,
   getChatUsers,
-  //getMessages,
   getCurrentUserId,
   getCurrentChat,
   getChatApp,
-  getSelectedUsers,
-  getUsersForAddInChat
+  getSelectedUsers
 } from "../../redux/selectors/index";
 
 const mapStateToProps = store => {
   return {
-    //usersForAdd: getUsersForAddInChat(store),
     users: getUsers(store),
     chatUsers: getChatUsers(store),
     currentUserId: getCurrentUserId(store),
@@ -30,7 +28,8 @@ const mapStateToProps = store => {
 const mapDispatchToProps = dispatch => {
   return {
     selectUser: userId => dispatch(selectUser(userId)),
-    unSelectUser: userId => dispatch(unSelectUser(userId))
+    unSelectUser: userId => dispatch(unSelectUser(userId)),
+    addUserstoChat: () => dispatch(addUserstoChat())
   };
 };
 
@@ -46,22 +45,26 @@ class UsersListContainer extends Component {
   };
 
   render() {
-    const { selectedUsers, users, chatUsers } = this.props;
-    const chatUsersObj=_.keyBy(chatUsers.items, 'id');
+    const { selectedUsers, users, chatUsers, addUserstoChat } = this.props;
+    const chatUsersObj = _.keyBy(chatUsers.items, "id");
 
-    if(users.isFetching==true || chatUsers.isFetching==true || chatUsers.fetched==false ){
-      return <div>users list is loading</div>
+    if (
+      users.isFetching == true ||
+      chatUsers.isFetching == true ||
+      chatUsers.fetched == false
+    ) {
+      return <div>users list is loading</div>;
     }
 
     let usersItemsForAdd = users;
-    usersItemsForAdd.items= users.items.filter(item => !chatUsersObj[item.id]);
-
+    usersItemsForAdd.items = users.items.filter(item => !chatUsersObj[item.id]);
 
     return (
       <UsersList
         users={usersItemsForAdd}
         selectedUsers={selectedUsers}
         onClickUser={this.onClickHandler}
+        addUserstoChat={addUserstoChat}
       />
     );
   }
