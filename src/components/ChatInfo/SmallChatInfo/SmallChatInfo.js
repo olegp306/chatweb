cdd2c5a1274dd2cb6c22710985057511e4c493d9
oleg <olegp306@gmail.com>
2019-06-30 13:14:03 +0300

    add chatType container

--- src/components/ChatInfo/SmallChatInfo/SmallChatInfo.js
+++ src/components/ChatInfo/SmallChatInfo/SmallChatInfo.js
@@ -1,17 +1,10 @@
 import React, { Component } from "react";
-
-const chatOnWarningTypeId="2768909697000" //чат по замечание
-const chatOnRequest="2768031944000" //чат по заявке
-
+import { chatTypes } from "../../../const/const";
 
 export default class SmallChat extends Component {
-  
   componentDidMount() {}
 
-  componentWillReceiveProps(nextProps) {
-  }
-
-
+  componentWillReceiveProps(nextProps) {}
   handOnChatClick = e => {
     if (e.target.id) {
       this.state.changeCurrentChatFn(e.target.id);
@@ -20,41 +13,59 @@ export default class SmallChat extends Component {
 
   render() {
     const {
-      chat,      
+      chat,
       isCurrentChat,
       unreadMessagesCount,
       onClickChat
     } = this.props;
-    const lastMessage= (chat.lastMessage &&  chat.lastMessage.text?  chat.lastMessage.text.slice(0, 30) + ( chat.lastMessage.text.length>40 ? " ...":"")  : "" );
+    const lastMessage =
+      chat.lastMessage && chat.lastMessage.text
+        ? chat.lastMessage.text.slice(0, 30) +
+          (chat.lastMessage.text.length > 40 ? " ..." : "")
+        : "";
     if (!chat) {
       return <div>Chats are downloading</div>;
     } else {
       return (
         <li
-          className={isCurrentChat == true ? (chat.chatTypeId== chatOnWarningTypeId ? "active-warning":"active") : ""}
-          //id={chat.id}
+          className={
+            isCurrentChat == true
+              ? chat.chatTypeId == chatOnWarningTypeId
+                ? "active-warning"
+                : "active"
+              : ""
+          }
         >
-          <a className= { chat.chatTypeId== chatOnWarningTypeId ? "small-chat-text-warning " : "small-chat-text"} 
-            //id={chat.id}
+          <a
+            className={
+              chat.chatTypeId == chatOnWarningTypeId
+                ? "small-chat-text-warning "
+                : "small-chat-text"
+            }
             href="#"
-            onClick={()=>onClickChat(chat)}
+            onClick={() => onClickChat(chat)}
           >
             {chat.name}
             <span
               className={
-                unreadMessagesCount == 0
-                  ? "hidden"
-                  : "unread-message-count"
+                unreadMessagesCount == 0 ? "hidden" : "unread-message-count"
               }
-            >              
+            >
               {unreadMessagesCount}
             </span>
-            <div></div>
-            
-            <span className="last-message-text-in-sm-chat">
-              {lastMessage} 
-            </span>
-           
+            <div />
+
+            <span className="last-message-text-in-sm-chat">{lastMessage}</span>
+            <div>
+              <span
+                className={
+                  "type-chat-text-in-sm-chat-" +
+                  chatTypes[chat.chatTypeId].color
+                }
+              >
+                {chatTypes[chat.chatTypeId].name}
+              </span>
+            </div>
           </a>
         </li>
       );
